a31b218b7cbe850de37c8482c62e95c14ecff4c6
tadashi-aikawa <syou.maman@gmail.com>
2019-12-31 00:49:56 +0900

    :tada: First commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-row align="center" justify="center">
+        <v-img
+          src="https://github.com/tadashi-aikawa/togowl/raw/master/public/icon.png"
+        ></v-img>
+      </v-row>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+@Component({
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+})
+class Root extends Vue {}
+
+export default Root
+</script>
