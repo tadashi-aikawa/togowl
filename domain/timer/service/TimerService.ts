6b07ef14cb134d89e6d1703483e679b6300bc764
tadashi-aikawa <syou.maman@gmail.com>
2020-01-13 18:31:32 +0900

    :hammer: Refactoring

--- domain/timer/service/TimerService.ts
+++ domain/timer/service/TimerService.ts
@@ -4,18 +4,18 @@ import { Either } from '~/node_modules/fp-ts/lib/Either';
 import { Project } from '~/domain/timer/entity/Project';
 
 export interface TimerEventListener {
-  onStartSubscribe?: () => void;
-  onEndSubscribe?: () => void;
-  onError?: (err: TogowlError) => void;
-  onInsertEntry?: (entry: Entry) => void;
-  onUpdateEntry?: (entry: Entry) => void;
-  onDeleteEntry?: (entry: Entry) => void;
-  onUpdateProject?: () => void;
+  onStartSubscribe?(): void;
+  onEndSubscribe?(): void;
+  onError?(err: TogowlError): void;
+  onInsertEntry?(entry: Entry): void;
+  onUpdateEntry?(entry: Entry): void;
+  onDeleteEntry?(entry: Entry): void;
+  onUpdateProject?(): void;
 }
 
 export interface TimerService {
-  fetchCurrentEntry: () => Promise<Either<TogowlError, Entry | null>>;
-  stopEntry: (entry: Entry) => Promise<Either<TogowlError, Entry>>;
+  fetchCurrentEntry(): Promise<Either<TogowlError, Entry | null>>;
+  stopEntry(entry: Entry): Promise<Either<TogowlError, Entry>>;
   fetchProjects(): Promise<Either<TogowlError, Project[]>>;
   terminate(): void;
 }
