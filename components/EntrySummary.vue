d5dbe53503d89295523e737fb52a954e799250ac
tadashi-aikawa <syou.maman@gmail.com>
2020-01-29 12:54:41 +0900

    :hammer: Refactoring

--- components/EntrySummary.vue
+++ components/EntrySummary.vue
@@ -1,26 +1,15 @@
 <template>
   <div>
-    <div class="entry-title" :style="{ width: width }" v-text="title" />
-    <div class="entry-subtitle">
-      <v-avatar v-if="entry.projectCategory" size="14px">
-        <ProjectCategoryIcon :project-category="entry.projectCategory" />
-      </v-avatar>
-      <span v-text="projectCategoryName" />
-      <span style="margin: 0 2px;">></span>
-      <v-avatar v-if="entry.project" size="14px">
-        <ProjectIcon :project="entry.project" />
-      </v-avatar>
-      <span v-text="projectName" />
-    </div>
+    <div class="entry" :style="{ width: width }" v-text="title" />
+    <ProjectSummary :project="entry.project" />
   </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from '~/node_modules/nuxt-property-decorator';
 import { Entry } from '~/domain/timer/entity/Entry';
-import ProjectIcon from '~/components/ProjectIcon.vue';
-import ProjectCategoryIcon from '~/components/ProjectCategoryIcon.vue';
+import ProjectSummary from '~/components/ProjectSummary.vue';
 
-@Component({ components: { ProjectIcon, ProjectCategoryIcon } })
+@Component({ components: { ProjectSummary } })
 class EntrySummary extends Vue {
   @Prop()
   entry: Entry;
@@ -31,38 +20,17 @@ class EntrySummary extends Vue {
   get title(): string {
     return this.entry.description;
   }
-
-  get projectName(): string {
-    return this.entry.project?.nameWithoutBracket ?? '';
-  }
-
-  get projectCategoryName(): string {
-    return this.entry.projectCategory?.nameWithoutBracket ?? '';
-  }
-
-  get subtitle(): string {
-    const project = this.entry.project?.nameWithoutBracket ?? '';
-    const projectCategory = this.entry.projectCategory?.nameWithoutBracket ?? '';
-    return projectCategory ? `${projectCategory} > ${project}` : project;
-  }
 }
 export default EntrySummary;
 </script>
 
 <style scoped>
-.entry-title {
+.entry {
   font-size: 100% !important;
   color: white;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
   padding-bottom: 3px;
-}
-.entry-subtitle {
-  font-size: 75%;
-  color: darkgrey;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
 }
 </style>
---
+++ components/ProjectSummary.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="root">
+    <template v-if="project.category">
+      <v-avatar size="14px">
+        <ProjectCategoryIcon :project-category="project.category" />
+      </v-avatar>
+      <span v-text="projectCategoryName" />
+      <span style="margin: 0 2px;">></span>
+    </template>
+    <v-avatar v-if="project" size="14px">
+      <ProjectIcon :project="project" />
+    </v-avatar>
+    <span v-text="projectName" />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from '~/node_modules/nuxt-property-decorator';
+import ProjectIcon from '~/components/ProjectIcon.vue';
+import ProjectCategoryIcon from '~/components/ProjectCategoryIcon.vue';
+import { Project } from '~/domain/timer/entity/Project';
+
+@Component({ components: { ProjectIcon, ProjectCategoryIcon } })
+class ProjectSummary extends Vue {
+  @Prop()
+  project: Project;
+
+  @Prop()
+  width: string;
+
+  get projectName(): string {
+    return this.project?.nameWithoutBracket ?? '';
+  }
+
+  get projectCategoryName(): string {
+    return this.project.category?.nameWithoutBracket ?? '';
+  }
+}
+export default ProjectSummary;
+</script>
+
+<style scoped>
+.root {
+  font-size: 75%;
+  color: darkgrey;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
