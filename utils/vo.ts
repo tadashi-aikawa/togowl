b77355040928d8241b31ff8c9ed3ac49f87e9955
tadashi-aikawa <syou.maman@gmail.com>
2020-02-19 17:59:13 +0900

    :hammer: Refactoring utils/vo

--- utils/vo.ts
+++ utils/vo.ts
@@ -5,14 +5,14 @@ interface ValueObjectProps {
 }
 
 export abstract class AbstractValueObject<T> {
-  protected readonly _value: T | undefined;
+  protected readonly _value: T;
 
-  protected constructor(_value?: T) {
+  protected constructor(_value: T) {
     this._value = Object.freeze(_value);
   }
 
   equals(vo?: AbstractValueObject<T>): boolean {
-    if (vo == null || vo._value == null) {
+    if (vo == null) {
       return false;
     }
     return shallowEqual(this._value, vo._value);
@@ -21,7 +21,7 @@ export abstract class AbstractValueObject<T> {
 
 export abstract class ValueObject<T extends ValueObjectProps> extends AbstractValueObject<T> {}
 
-export abstract class PrimitiveValueObject<T> extends ValueObject<T> {
+export abstract class PrimitiveValueObject<T> extends AbstractValueObject<T> {
   get value(): T {
     return this._value!;
   }
